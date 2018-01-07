4ae670996b2720d427cca654ea17b5766d6e85e0
Krishna Mahadevan <krishnabm@users.noreply.github.com>
2018-01-07 13:00:58 +0530

    v1.0

---
+++ LogicAppMgt.ps1
@@ -0,0 +1,34 @@
+param(
+[switch]$enable,
+[switch]$disable
+)
+
+$subscription = "GD-EXT-FARMSLAND-Ravivd-Vibansal"
+$resourceGroup = "rg-aue-ms-test-native"
+$isDisableMode = 0
+
+az account set -s "$subscription"
+
+$list = az resource list --resource-group "$resourceGroup" --resource-type 'Microsoft.Logic/workflows' | ConvertFrom-Json
+
+If($disable)
+{
+	foreach ($logicApp in $list) {
+		$name = $logicApp.name
+		echo "Disabling $name"
+		az resource invoke-action --action disable  --name "$name" -g "$resourceGroup" --resource-type "Microsoft.Logic/workflows"
+	}
+}
+elseIf($enable)
+{
+	foreach ($logicApp in $list) {
+		$name = $logicApp.name
+		echo "Enabling $name"
+		az resource invoke-action --action enable  --name "$name" -g "$resourceGroup" --resource-type "Microsoft.Logic/workflows"
+	}
+}
+else
+{
+	echo "Please specify -enable or -disable switch"
+}
+exit 0
